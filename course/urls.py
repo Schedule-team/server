a669c39e8ec2de98afe4797d98161439fd8510f6
TianKai Ma <tiankaima@163.com>
2024-01-17 07:51:26 +0800

    chore: remove test.json

--- course/urls.py
+++ course/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path("api/course/", api.query_course_all, name="index"),
-    path("api/course/<int:id>", api.query_course, name="course"),
-    path("api/lesson/", api.query_lesson_all, name="lesson"),
-    path("api/lesson/<int:id>", api.query_lesson, name="lesson"),
-    path("api/teacher/", api.query_teacher_all, name="teacher"),
-    path("api/teacher/<int:id>", api.query_teacher, name="teacher"),
-    path("api/semester/", api.query_semester_all, name="semester"),
-    path("api/semester/<int:id>", api.query_semester, name="semester"),
+    path("api/course/", api.query_course_all, name="api_course_all"),
+    path("api/course/<int:id>", api.query_course, name="api_course"),
+    path("api/lesson/", api.query_lesson_all, name="api_lesson_all"),
+    path("api/lesson/<int:id>", api.query_lesson, name="api_lesson"),
+    path("api/teacher/", api.query_teacher_all, name="api_teacher_all"),
+    path("api/teacher/<int:id>", api.query_teacher, name="api_teacher"),
+    path("api/semester/", api.query_semester_all, name="api_semester_all"),
+    path("api/semester/<int:id>", api.query_semester, name="api_semester"),
 ]
