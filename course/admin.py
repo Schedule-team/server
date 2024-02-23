0a0970e2917c923313c9081994c53f5860bbc282
tiankaima <tiankaima@163.com>
2024-02-23 22:53:19 +0800

    feat: add history to admin page

--- course/admin.py
+++ course/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from .models import *
 
@@ -6,8 +7,8 @@ admin.site.register(Course)
 admin.site.register(Teacher)
 admin.site.register(Semester)
 admin.site.register(Location)
-admin.site.register(Lecture)
-admin.site.register(Exam)
-admin.site.register(Lesson)
+admin.site.register(Lecture, SimpleHistoryAdmin)
+admin.site.register(Exam, SimpleHistoryAdmin)
+admin.site.register(Lesson, SimpleHistoryAdmin)
 admin.site.register(Homework)
 admin.site.register(EditableTextModel)
